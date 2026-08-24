7c760581b4046415845ca5429191cd7f3937f004
Ashley Petgrave <ashley.petgrave@bison.howard.edu>
2026-08-24 16:47:03 -0400

    Create HelloWorld.java

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Prints the required greeting for Assignment 1.
+ *
+ * @author Ashley Petgrave
+ */
+public class HelloWorld {
+    /**
+     * Application entry point.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
